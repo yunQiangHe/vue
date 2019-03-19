0f99cba9e725b89f6583083bdb6ac95d2cee32f2
heyunqiang <heyunqiang99@163.com>
2019-03-20 00:14:02 +0800

    vue学习第一天

---
+++ hello-vue/src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <h1>{{ sonMsg }}</h1>
+    <button class="btn" @click="reverseMsg">reverseMsg</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      sonMsg: this.msg
+    }
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy");
+  },
+  destroyed() {
+    console.log("destroyed");
+  },
+  methods: {
+   reverseMsg(){
+     this.sonMsg = this.sonMsg.split("").reverse().join("");
+   }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.btn{
+  padding: 5px 8px;
+  margin:5px 2px;
+}
+</style>
---
+++ hello-vue/src/components/Test.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="test">
+    <p>Original message - {{ message }}</p>
+    <p>Computed resvered message - {{ resvereMessage }}</p>
+    <p>如果你不希望有缓存，请用方法来替代。- {{ resvereMessage2() }}</p>
+    <p>
+        Ask a yes/no question:
+        <input v-model='question'>
+    </p>
+    <p>{{ answer }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message:"ABC",
+            question:"",
+            answer:"I cannot give you an answer until you ask a question!"
+        }
+    },
+    computed: {
+        resvereMessage(){
+            return this.message.split("").reverse().join("");
+        },
+        now(){
+            return Date.now();
+        }
+    },
+    methods: {
+        resvereMessage2(){
+            return this.message.split("").reverse().join("");
+        }
+    },
+};
+</script>
+
+<style scoped>
+.test{
+    background: darkcyan 
+}
+</style>
