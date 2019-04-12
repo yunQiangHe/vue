0c7f99ceef1b191935faa5001d27db0454d8baea
heyunqiang <heyunqiang99@163.com>
2019-04-12 23:56:19 +0800

    vue单页面

---
+++ my-app/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h1>{{ msg }}</h1>
+    <input type="text" v-model="info">
+    <button @click="handClick">添加</button>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ul>
+      <!-- <todo-item v-for="item in lists" :item="item" :key="item">  属性传递参数 -->
+      <todo-item v-for="item in lists" :key="item">
+          <!-- <span v-slot:item style="font-size:20px;"> {{ item }} </span> 插槽  v-slot can only be used on components or <template>. -->
+            <template  v-slot:item>  <!-- 具名插槽 -->
+                <!-- <span slot="item" style="font-size:20px;"> {{ item }} </span>  -->
+                <span  style="font-size:20px;"> {{ item }} </span> <!-- 插槽 -->
+            </template>
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from "./components/TodoItem.vue";
+
+export default {
+  name: "app",
+  components: {
+    // HelloWorld,
+    TodoItem
+  },
+  data() {
+    return {
+      msg:"Hello vue , go on!",
+      lists:[],
+      info:""
+    }
+  },
+  methods: {
+    handClick(){
+      this.lists.push(this.info);
+      this.info = "";
+    }
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ my-app/src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li class="item">
+         <!-- {{ item }} -->
+         <input type="checkbox" v-model="checked">
+         <slot name="item" v-bind="{checked}"></slot>  <!-- 插槽 -->
+    </li>
+<!-- 补充知识点 插槽 -->
+</template>
+
+<script>
+export default {
+    props:[ 'item' ],
+    data() {
+        return {
+            checked: false
+        }
+    },
+}
+</script>
+
+<style scoped>
+.item{
+    color: red;
+}
+</style>
+
